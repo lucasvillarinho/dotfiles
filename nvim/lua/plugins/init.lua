62e4b59e0a717b9fe92cdbeeb5e23c2e803bfe9c
Lucas Villarinho <lucas.sbv5@yahoo.com.br>
2024-08-17 08:27:42 -0300

    fix: Remove dashboard

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -11,15 +11,4 @@ return {
     end,
   },
 
-  {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        -- config
-      }
-    end,
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
-  }
-
 }
