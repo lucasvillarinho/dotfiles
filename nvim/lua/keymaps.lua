7bfe3107440f423fd18af6bebb3f043f7a529342
Lucas Villarinho <lucasvillarinho1@gmail.com>
2024-09-17 08:58:31 -0300

    feat(nvim): Add indent-blankline

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,21 +4,11 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
 -- Neotree
-vim.api.nvim_set_keymap('n', '<C-e>', ':Neotree toggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-e>', ':Neotree toggle<CR>:Neotree focus<CR>', { noremap = true, silent = true })
 
 -- Lazy
 vim.api.nvim_set_keymap('n', '<leader>rl', ':lua ReloadConfig()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ll', ':Lazy<CR>', { noremap = true, silent = true })
-
-
--- Tabby
-vim.api.nvim_set_keymap("n", "<leader>ta", ":$tabnew<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>to", ":tabonly<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>tn", ":tabn<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
 
 -- Nvim
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
@@ -31,10 +21,6 @@ vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
 vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
 vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
 
-vim.keymap.set('n', '<C-Left>', require('smart-splits').resize_left)
-vim.keymap.set('n', '<C-Right>', require('smart-splits').resize_right)
-vim.keymap.set('n', '<C-Up>', require('smart-splits').resize_up)
-vim.keymap.set('n', '<C-Down>', require('smart-splits').resize_down)
-
 vim.keymap.set('n', '<leader>sv', ':vsplit<CR>', { noremap = true, silent = true })
+
 
