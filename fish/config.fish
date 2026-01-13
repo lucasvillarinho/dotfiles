8809e71b8cfae26b3d3ece5d1a10a3a6dec66d3f
Lucas Villarinho <lucasvillarinho1@gmail.com>
2026-01-13 20:26:56 -0300

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
 starship init fish | source
 
 fzf --fish | source
+
+alias l="ls -la"
+alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+set -g zoxide_cmd cf
+
+export FZF_DEFAULT_OPTS='--height 40%'
